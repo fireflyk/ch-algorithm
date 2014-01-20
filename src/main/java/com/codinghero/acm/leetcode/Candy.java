83cae88f3fb9eb8118e18877ff7ebe880d2eef23
fireflyk <liutong628@gmail.com>
2014-01-20 16:57:30 +0800

    candy

--- src/main/java/com/codinghero/acm/leetcode/Candy.java
+++ src/main/java/com/codinghero/acm/leetcode/Candy.java
@@ -7,26 +7,17 @@ public class Candy {
 
 		int[] nums = new int[ratings.length];
 		nums[0] = 1;
-		int min = 1;
 		for (int i = 1; i < nums.length; i++) {
 			if (ratings[i] > ratings[i - 1])
 				nums[i] = nums[i - 1] + 1;
-			else if (ratings[i] == ratings[i - 1])
+			else
 				nums[i] = 1;
-			else {
-				nums[i] = nums[i - 1] - 1;
-				if (nums[i] < min)
-					min = nums[i];
-			}
 		}
-
-		int addNum = 1 - min;
-		nums[nums.length - 1] += addNum;
+		
 		int sum = nums[nums.length - 1];
 		for (int i = nums.length - 2; i >= 0; i--) {
-			if(ratings[i]==ratings[i+1])
-				
-			nums[i] += addNum;
+			if (ratings[i] > ratings[i + 1] && nums[i] <= nums[i + 1])
+				nums[i] = nums[i + 1] + 1;
 			sum += nums[i];
 		}
 
