52aa4ee4c846129efa24319871abd03f4fa30386
fireflyk <liutong628@gmail.com>
2014-01-27 23:54:27 +0800

    ZigZagConversion

---
+++ src/test/java/com/codinghero/acm/leetcode/ZigZagConversionTest.java
@@ -0,0 +1,14 @@
+package com.codinghero.acm.leetcode;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class ZigZagConversionTest {
+	@Test
+	public void test() {
+		ZigZagConversion instance = new ZigZagConversion();
+		Assert.assertEquals("AB", instance.convert("AB", 2));
+		Assert.assertEquals("PAHNAPLSIIGYIR", instance.convert("PAYPALISHIRING", 3));
+	}
+}
