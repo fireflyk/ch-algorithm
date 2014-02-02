88352230f5ec4563690efd5e844c995d69949f30
fireflyk <liutong628@gmail.com>
2014-02-02 18:19:10 +0800

    Graph. Prim.

--- src/main/java/com/codinghero/acm/poj/Poj1258.java
+++ src/main/java/com/codinghero/acm/poj/Poj1258.java
@@ -1,9 +1,8 @@
 package com.codinghero.acm.poj;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashSet;
+import java.util.Scanner;
 
 /**
  * 
@@ -18,23 +17,20 @@ import java.util.HashSet;
  */
 public class Poj1258 {
 
-	// public static final int UNCONNECTED_VERTEX = -1;
-
-	// private int[][] graph;
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		long[][] graph = getInput();
-		System.out.println(prim(graph));
+		Scanner cin = new Scanner(System.in);
+		while (cin.hasNext()) {
+			long[][] graph = getInput(cin);
+			System.out.println(prim(graph));
+		}
 	}
 	
-	public static long[][] getInput() throws NumberFormatException, IOException {
-		BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
-		final int n = Integer.parseInt(cin.readLine());
+	public static long[][] getInput(Scanner cin) throws NumberFormatException, IOException {
+		final int n = cin.nextInt();
 		long[][] graph = new long[n][n];
 		for (int i = 0; i < n; i++) {
-			String[] nums = cin.readLine().split(" ");
-			for (int j = 0; j < nums.length; j++) {
-				graph[i][j] = Long.parseLong(nums[j]);
+			for (int j = 0; j < n; j++) {
+				graph[i][j] = cin.nextLong();
 			}
 		}
 		return graph;
@@ -48,17 +44,17 @@ public class Poj1258 {
 		return prim(graph, unSelectedSet, new Long[graph.length], 0);
 	}
 
-	// LinkedList<Integer> selected
 	private static long prim(long[][] graph, HashSet<Integer> unSelectedSet, Long[] minCost, int prevSelect) {
 		if (unSelectedSet.isEmpty())
 			return 0;
 		Integer curSelect = null;
 		for (Integer unSelect : unSelectedSet) {
+			// visit all edge of the new selected vertex
 			if (minCost[unSelect] == null
 					|| graph[prevSelect][unSelect] < minCost[unSelect]) {
 				minCost[unSelect] = graph[prevSelect][unSelect];
 			}
-
+			// find minimum edge on this round
 			if (curSelect == null || minCost[curSelect] > minCost[unSelect]) {
 				curSelect = unSelect;
 			}
