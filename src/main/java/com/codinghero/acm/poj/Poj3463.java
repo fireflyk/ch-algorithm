d4de4219abd667507a3a8fad1afaed09d7756002
fireflyk <liutong628@gmail.com>
2014-02-04 15:31:07 +0800

    temp version

--- src/main/java/com/codinghero/acm/poj/Poj3463.java
+++ src/main/java/com/codinghero/acm/poj/Poj3463.java
@@ -16,28 +16,31 @@ import java.util.LinkedList;
  */
 public class Poj3463 {
 
+	//private Long[][] min
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		Poj3463 instance = new Poj3463();
 		StreamTokenizer cin = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
 		cin.nextToken();
 		final int caseNumber = (int) cin.nval;
 		for (int i = 0; i < caseNumber; i++) {
-			ArrayList<LinkedList<Edge>> graph = getInput(cin);
+			ArrayList<LinkedList<Edge>> graph = instance.getInput(cin);
 			cin.nextToken();
 			final int orig = (int) cin.nval;
 			cin.nextToken();
 			final int dest = (int) cin.nval;
-			System.out.println(dijkstra(graph, orig, dest));
+			instance.dijkstra(graph, orig, dest);
 		}
 	}
 	
-	public static ArrayList<LinkedList<Edge>> getInput(StreamTokenizer cin) throws NumberFormatException, IOException {
+	public ArrayList<LinkedList<Edge>> getInput(StreamTokenizer cin) throws NumberFormatException, IOException {
 		cin.nextToken();
 		final int vertexNumber = (int) cin.nval;
 		cin.nextToken();
 		final int edgeNumber = (int) cin.nval;
 		// initial the graph 
 		ArrayList<LinkedList<Edge>> graph = new ArrayList<LinkedList<Edge>>();
-		for (int i = 0; i < vertexNumber; i++) {
+		for (int i = 0; i < vertexNumber + 1; i++) {
 			graph.add(new LinkedList<Edge>());
 		}
 		// add edge
@@ -47,98 +50,172 @@ public class Poj3463 {
 			cin.nextToken();
 			final int to = (int) cin.nval;
 			cin.nextToken();
-			final int distance = (int) cin.nval;
-			Edge edge = new Edge(to, distance);
+			final long distance = (int) cin.nval;
+			Edge edge = new Edge(from, to, distance);
 			LinkedList<Edge> edges = graph.get(from);
-			// very small number, leave it only
-			if (edges.size() == 0 || distance + 1 < edges.getFirst().distance) {
-				edges.addFirst(edge);
+			
+			Iterator<Edge> iter = edges.iterator();
+			Long min = null;
+			int state = 0;
+			while (iter.hasNext()) {
+				Edge curEdge = iter.next();
+				if (curEdge.to == to) {
+					if (distance <= curEdge.distance) {
+						min = distance;
+						state = 1;
+						break;
+					} else if (distance == curEdge.distance + 1) {
+						min = curEdge.distance;
+						state = 1;
+						break;
+					}
+				}
 			}
-			// smaller than the smallest one, remove the tail
-			else if (distance + 1 == edges.getFirst().distance) {
+			
+			if (state == 0) {
 				edges.addFirst(edge);
-				Iterator<Edge> iter = edges.iterator();
+			} else if (state == 1) {
 				while (iter.hasNext()) {
-					if (iter.next().distance > distance + 1) {
+					Edge curEdge = iter.next();
+					if (curEdge.to == to && min + 1 < curEdge.distance) {
 						iter.remove();
 					}
 				}
-			} 
-			// equals the smallest one
-			else if (distance == edges.getFirst().distance) {
-				edges.addFirst(edge);
-			}
-			// equals the biggest one
-			else if (distance == edges.getFirst().distance + 1) {
-				edges.addLast(edge);
+				if (min == distance) {
+					edges.addFirst(edge);
+				} else if (min + 1 == distance) {
+					edges.addLast(edge);
+				}
 			}
 		}
 		return graph;
 	}
 	
-	public static int dijkstra(ArrayList<LinkedList<Edge>> graph, final int orig, final int dest) {
+	public void dijkstra(ArrayList<LinkedList<Edge>> graph, final int orig, final int dest) {
 		HashSet<Integer> unSelectedSet = new HashSet<Integer>();
 		for (int i = 1; i < graph.size(); i++) {
 			unSelectedSet.add(i);
 		}
-		return dijkstra(graph, unSelectedSet, new ArrayList<LinkedList<Long>>(graph.size()), orig, dest);
+		unSelectedSet.remove(orig);
+		ArrayList<LinkedList<Long>> minCosts = new ArrayList<LinkedList<Long>>();
+		for (int i = 1; i <= graph.size(); i++) {
+			minCosts.add(new LinkedList<Long>());
+		}
+		minCosts.get(orig).add(0L);
+		dijkstra(graph, unSelectedSet, minCosts, orig, dest, 0L);
 	}
 
-	private static int dijkstra(ArrayList<LinkedList<Edge>> graph, HashSet<Integer> unSelectedSet, ArrayList<LinkedList<Long>> minCosts, int prevSelect, final int dest) {
+	private void dijkstra(ArrayList<LinkedList<Edge>> graph, HashSet<Integer> unSelectedSet, ArrayList<LinkedList<Long>> minCosts, final int from, final int to, Long prevMin) {
 		if (unSelectedSet.isEmpty())
-			return 0;
+			return;
 		Integer curSelect = null;
-		for (Edge edge : graph.get(prevSelect)) {
-			if (unSelectedSet.contains(edge.to)) {
-				calculate(edge, minCosts, prevSelect);
-			}
-		}
 		Long min = null;
-		for (Integer unSelect : unSelectedSet) {
-			for (Long distance : minCosts.get(unSelect)) {
-				if (min == null || min > distance) {
-					min = distance;
-					curSelect = unSelect;
+		// iterate edge of the vertex
+		for (Edge edge : graph.get(from)) {
+			if (unSelectedSet.contains(edge.to)) {
+				// iterate cost of all the vertex cost
+				for (Long minCost : minCosts.get(edge.from)) {
+					long temp = edge.distance + minCost;
+					//minCosts.get(edge.to).add(temp);
+					
+					// start 
+					Iterator<Long> iter = minCosts.get(edge.to).iterator();
+					Long innerMin = null;
+					int state = 0;
+					while (iter.hasNext()) {
+						long distance = iter.next();
+						if (temp <= distance) {
+							innerMin = temp;
+							state = 1;
+							break;
+						} else if (temp == distance + 1) {
+							innerMin = distance;
+							state = 1;
+							break;
+						}
+					}
+					if (state == 0) {
+						minCosts.get(edge.to).add(temp);
+					} else if (state == 1) {
+						while (iter.hasNext()) {
+							Long distance = iter.next();
+							if (innerMin + 1 < distance) {
+								iter.remove();
+							}
+						}
+						if (min == temp) {
+							minCosts.get(edge.to).addFirst(temp);
+						} else if (min + 1 == temp) {
+							minCosts.get(edge.to).addLast(temp);
+						}
+					}
+					// end
+				}
+				// calculate minimum
+				Long min = null;
+				for (Long distance : minCosts.get(edge.to)) {
+					if (min == null || min > distance) {
+						min = distance;
+					}
+				}
+				// remove the bigger ones
+				Iterator<Long> iter = minCosts.get(edge.to).iterator();
+				while (iter.hasNext()) {
+					if (min + 1 < iter.next()) {
+						iter.remove();
+					}
 				}
 			}
 		}
+//		Long min = null;
+//		for (Integer unSelect : unSelectedSet) {
+//			for (Long distance : minCosts.get(unSelect)) {
+//				if ((min == null || min > distance) && distance >= prevMin) {
+//					min = distance;
+//					curSelect = unSelect;
+//				}
+//			}
+//		}
 		
-		if (curSelect == dest) {
-			return minCosts.get(curSelect).size();
-		}
+//		if (curSelect == dest) {
+//			return minCosts.get(curSelect).size();
+//		}
 		
-		unSelectedSet.remove(curSelect);
-		return dijkstra(graph, unSelectedSet, minCosts, curSelect, dest);
+//		unSelectedSet.remove(curSelect);
+		dijkstra(graph, unSelectedSet, minCosts, curSelect, dest, min);
 	}
 	
-	private static void calculate(Edge edge, ArrayList<LinkedList<Long>> minCosts, int prevSelect) {
-		// calculate all possible distance
-		for (Long minCost : minCosts.get(prevSelect)) {
-			long temp = edge.distance + minCost;
-			minCosts.get(edge.to).add(temp);
-		}
-		// calculate minimum
-		Long min = null;
-		for (Long distance : minCosts.get(edge.to)) {
-			if (min == null || min > distance) {
-				min = distance;
-			}
-		}
-		// remove the bigger ones
-		Iterator<Long> iter = minCosts.get(edge.to).iterator();
-		while (iter.hasNext()) {
-			if (min + 1 < iter.next()) {
-				iter.remove();
-			}
-		}
-	}
+//	private void calculate(Edge edge, ArrayList<LinkedList<Long>> minCosts) {
+//		// calculate all possible distance
+//		for (Long minCost : minCosts.get(edge.from)) {
+//			long temp = edge.distance + minCost;
+//			minCosts.get(edge.to).add(temp);
+//		}
+//		// calculate minimum
+//		Long min = null;
+//		for (Long distance : minCosts.get(edge.to)) {
+//			if (min == null || min > distance) {
+//				min = distance;
+//			}
+//		}
+//		// remove the bigger ones
+//		Iterator<Long> iter = minCosts.get(edge.to).iterator();
+//		while (iter.hasNext()) {
+//			if (min + 1 < iter.next()) {
+//				iter.remove();
+//			}
+//		}
+//	}
 	
 	private static class Edge {
+		private int from;
 		private int to;
 		private long distance;
-		public Edge(int to, long distance) {
+		public Edge(int from, int to, long distance) {
+			this.from = from;
 			this.to = to;
 			this.distance = distance;
 		}
 	}
+	
 }
