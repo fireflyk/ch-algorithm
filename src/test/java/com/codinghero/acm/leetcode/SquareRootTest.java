591caba42f071ecf9cd1a58b6f018240831ffada
liutong01 <liutong01@82AAB9C24AA1C09.internal.baidu.com>
2014-02-12 21:46:03 +0800

    newton square root

--- src/test/java/com/codinghero/acm/leetcode/SquareRootTest.java
+++ src/test/java/com/codinghero/acm/leetcode/SquareRootTest.java
@@ -19,4 +19,19 @@ public class SquareRootTest {
 		Assert.assertEquals(3, new SquareRoot().sqrt(9));
 		Assert.assertEquals(46339, new SquareRoot().sqrt(2147395599));
 	}
+	
+	@Test
+	public void testSqrt2() {
+		Assert.assertEquals(0, new NewtonSquareRoot().sqrt(0));
+		Assert.assertEquals(1, new NewtonSquareRoot().sqrt(1));
+		Assert.assertEquals(1, new NewtonSquareRoot().sqrt(2));
+		Assert.assertEquals(1, new NewtonSquareRoot().sqrt(3));
+		Assert.assertEquals(2, new NewtonSquareRoot().sqrt(4));
+		Assert.assertEquals(2, new NewtonSquareRoot().sqrt(5));
+		Assert.assertEquals(2, new NewtonSquareRoot().sqrt(6));
+		Assert.assertEquals(2, new NewtonSquareRoot().sqrt(7));
+		Assert.assertEquals(2, new NewtonSquareRoot().sqrt(8));
+		Assert.assertEquals(3, new NewtonSquareRoot().sqrt(9));
+		Assert.assertEquals(46340, new NewtonSquareRoot().sqrt(2147483647));
+	}
 }
