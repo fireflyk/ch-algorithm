4c3d68a8c4e8b300f010e939389a7ae4b4d1b4db
liutong01 <liutong01@82AAB9C24AA1C09.internal.baidu.com>
2014-02-16 22:32:16 +0800

    two linked list iterator

--- src/main/java/com/codinghero/interview/google/TwoLinkedListIterator.java
+++ src/main/java/com/codinghero/interview/google/TwoLinkedListIterator.java
@@ -4,31 +4,16 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class TwoLinkedListIterator<T> implements Iterator<T> {
-
-	private static final class STATE {
-		private static final int NEXT_LIST1 = 0;
-		private static final int NEXT_LIST2 = 1;
-		private static final int NEXT_NONE = 2;
-	}
-	
-	// private LinkedList<T> list1 = new LinkedList<T>();
-	
-	// private LinkedList<T> list2 = new LinkedList<T>();
 	
 	private Iterator<T> iter1;
 	
 	private Iterator<T> iter2;
 	
 	private Iterator<T> curIter;
-	
-	// 1 list1, 2 list2, 3 end
-	// private int state;
 
 	public TwoLinkedListIterator(LinkedList<T> list1, LinkedList<T> list2) {
 		if (list1 == null || list2 == null)
 			throw new NullPointerException();
-		// this.list1 = list1;
-		// this.list2 = list2;
 		this.iter1 = list1.iterator();
 		this.iter2 = list2.iterator();
 	}
@@ -48,6 +33,7 @@ public class TwoLinkedListIterator<T> implements Iterator<T> {
 	@Override
 	public void remove() {
 		curIter.remove();
+		switchIterator();
 	}
 	
 	private void switchIterator() {
@@ -67,22 +53,4 @@ public class TwoLinkedListIterator<T> implements Iterator<T> {
 			curIter = iter2;
 		}
 	}
-	
-	/*
-	private void switchTo1() {
-		if (iter1.hasNext()) {
-			curIter = iter1;
-		} else if (iter2.hasNext()) {
-			curIter = iter2;
-		}
-	}
-	
-	private void switchTo2() {
-		if (iter2.hasNext()) {
-			curIter = iter2;
-		} else if (iter1.hasNext()) {
-			curIter = iter1;
-		}
-	}
-	*/
 }
