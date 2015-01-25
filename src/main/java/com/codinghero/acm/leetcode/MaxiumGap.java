2305112817969a07ec13009c5451de7693a6e101
Tong Liu <tliu@freewheel.tv>
2015-01-25 23:11:23 +0800

    Leetcode: Maxium Gap

---
+++ src/main/java/com/codinghero/acm/leetcode/MaxiumGap.java
@@ -0,0 +1,21 @@
+package com.codinghero.acm.leetcode;
+
+import java.util.Arrays;
+
+public class MaxiumGap {
+	public int maximumGap(int[] num) {
+		if (num.length < 2) {
+			return 0;
+		}
+		Arrays.sort(num);
+
+		int diff = num[1] - num[0];
+		for (int i = 1; i < num.length; i++) {
+			int newDiff = num[i] - num[i - 1];
+			if (newDiff > diff) {
+				diff = newDiff;
+			}
+		}
+		return diff;
+	}
+}
